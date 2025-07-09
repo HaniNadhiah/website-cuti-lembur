82a4525b31810032937a9f22ad9ccd41b98ec95e
HaniNadhiah <nadhiahhani2923@gmail.com>
2025-07-09 15:37:14 +0700

    base.blade.php

--- resources/views/dashboard/base.blade.php
+++ resources/views/dashboard/base.blade.php
@@ -53,10 +53,13 @@
       <div class="container">
         <div class="page-inner">
         <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
+        </div>
+
+        @include('dashboard.index')
 
           @include('dashboard.index')
         </div>
-
+    
         <!-- Card -->
         <div class="row">
           <div class="col-sm-6 col-md-3">
@@ -84,12 +87,11 @@
     @endif
       </div>
       <!-- End Card -->
-
-
-      @include('dashboard.footer')
+      
+    @include('dashboard.footer')
     </div>
-
   </div>
+  
   <!--   Core JS Files   -->
   <script src="assets/js/core/jquery-3.7.1.min.js"></script>
   <script src="assets/js/core/popper.min.js"></script>
@@ -134,6 +136,59 @@
     });
 
     $("#lineChart2").sparkline([99, 125, 122, 105, 110, 124, 115], {
+
+      @include('dashboard.footer')
+    </div>
+
+  </div>
+  <!--   Core JS Files   -->
+  <script src="assets/js/core/jquery-3.7.1.min.js"></script>
+  <script src="assets/js/core/popper.min.js"></script>
+  <script src="assets/js/core/bootstrap.min.js"></script>
+
+  <!-- jQuery Scrollbar -->
+  <script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+
+  <!-- Chart JS -->
+  <script src="assets/js/plugin/chart.js/chart.min.js"></script>
+
+  <!-- jQuery Sparkline -->
+  <script src="assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+
+  <!-- Chart Circle -->
+  <script src="assets/js/plugin/chart-circle/circles.min.js"></script>
+
+  <!-- Datatables -->
+  <script src="assets/js/plugin/datatables/datatables.min.js"></script>
+
+  <!-- jQuery Vector Maps -->
+  <script src="assets/js/plugin/jsvectormap/jsvectormap.min.js"></script>
+  <script src="assets/js/plugin/jsvectormap/world.js"></script>
+
+  <!-- Sweet Alert -->
+  <script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+
+  <!-- Kaiadmin JS -->
+  <script src="assets/js/kaiadmin.min.js"></script>
+
+  <!-- Kaiadmin DEMO methods, don't include it in your project! -->
+  <script src="assets/js/setting-demo.js"></script>
+  <script src="assets/js/demo.js"></script>
+  <script>
+    $("#lineChart").sparkline([102, 109, 120, 99, 110, 105, 115], {
+      type: "line",
+      height: "70",
+      width: "100%",
+      lineWidth: "2",
+      lineColor: "#f3545d",
+      fillColor: "rgba(243, 84, 93, .14)",
+    });
+
+      lineColor: "#177dff",
+      fillColor: "rgba(23, 125, 255, 0.14)",
+    });
+
+    $("#lineChart2").sparkline([99, 125, 122, 105, 110, 124, 115], {
       type: "line",
       height: "70",
       width: "100%",
@@ -152,6 +207,12 @@
     });
   </script>
 </body>
+<<<<<<< HEAD
+
+@yield('scripts')
+
+</html>
+=======
 
 @yield('scripts')
 
