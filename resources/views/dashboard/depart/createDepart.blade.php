36a21f40f59b395d2aaec0a5e4eae1051f08dc1e
HaniNadhiah <nadhiahhani2923@gmail.com>
2025-07-10 15:10:43 +0700

    createDepart.blade.php

--- resources/views/dashboard/depart/createDepart.blade.php
+++ resources/views/dashboard/depart/createDepart.blade.php
@@ -1,1 +1,22 @@
+@extends('dashboard.base')
+
+@section('no-cards')
+
+@section('content')
+<div class="container">
+    <h3>Tambah Departemen</h3>
+    <form action="{{ route('departments.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="name_departments" class="form-label">Nama Departemen</label>
+            <input type="text" name="name_departments" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label for="deskripsi" class="form-label">Deskripsi</label>
+            <textarea name="deskripsi" class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Simpan</button>
+        <a href="{{ route('departments.index') }}" class="btn btn-secondary">Batal</a>
+    </form>
+</div>
 
