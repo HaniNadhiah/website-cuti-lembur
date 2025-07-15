276c4ac0d4bb4513223c221da2ef1db8adeec0a3
HaniNadhiah <nadhiahhani2923@gmail.com>
2025-07-15 10:46:41 +0700

    base.blade.php

--- resources/views/dashboard/base.blade.php
+++ resources/views/dashboard/base.blade.php
@@ -6,6 +6,7 @@
   <title>Website Cuti/Lembur</title>
   <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
   <link rel="icon" href="assets/img/kaiadmin/favicon.ico" type="image/x-icon" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
   <!-- Fonts and icons -->
   <script src="assets/js/plugin/webfont/webfont.min.js"></script>
@@ -36,42 +37,39 @@
   <link rel="stylesheet" href="assets/css/demo.css" />
 </head>
 
-<body>
-  <div class="wrapper">
-    <!-- Sidebar -->
-    @include('dashboard.sidebar')
-    <!-- End Sidebar -->
+<div class="wrapper">
+  <!-- Sidebar -->
+  @include('dashboard.sidebar')
+  <!-- End Sidebar -->
 
-    <div class="main-panel">
-      @include('dashboard.header')
+  <div class="main-panel">
+    @include('dashboard.header')
 
+    <div class="main-content ">
       @yield('content')
 
-      <div class="content mt-4">
+      <div class="container">
         @if (Request::routeIs('home')) {{-- Ganti 'home' sesuai nama rute dashboard kamu --}}
-        <div class="container">
-          <div class="page-inner">
+        <div class="page-inner">
           <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4"></div>
+        </div>
 
-          @include('dashboard.index')
-          </div>
-
-          <!-- Card -->
-          <div class="row">
+        @include('dashboard.index')
+        <!-- Card -->
+        <div class="row">
           <div class="col-sm-6 col-md-3">
-            <div class="card card-stats card-round">
+          <div class="card card-stats card-round">
             <div class="card-body">
-              <div class="row align-items-center">
+            <div class="row align-items-center">
               <div class="col-icon">
-                <div class="icon-big text-center icon-primary bubble-shadow-small">
+              <div class="icon-big text-center icon-primary bubble-shadow-small">
                 <i class="fas fa-users"></i>
-                </div>
+              </div>
               </div>
               <div class="col col-stats ms-3 ms-sm-0">
-                <div class="numbers">
+              <div class="numbers">
                 <p class="card-category">Visitors</p>
                 <h4 class="card-title">1,294</h4>
-                </div>
               </div>
               </div>
             </div>
@@ -80,74 +78,73 @@
           </div>
         </div>
         </div>
-      @endif
-    </div>
-    <!-- End Card -->
+      </div>
+    @endif
   </div>
-  
+  <!-- End Card -->
   @include('dashboard.footer')
+</div>
 
-  <!--   Core JS Files   -->
-    <script src="assets/js/core/jquery-3.7.1.min.js"></script>
-    <script src="assets/js/core/popper.min.js"></script>
-    <script src="assets/js/core/bootstrap.min.js"></script>
+<script src="assets/js/core/jquery-3.7.1.min.js"></script>
+<script src="assets/js/core/popper.min.js"></script>
+<script src="assets/js/core/bootstrap.min.js"></script>
 
-    <!-- jQuery Scrollbar -->
-    <script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+<!-- jQuery Scrollbar -->
+<script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
 
-    <!-- Chart JS -->
-    <script src="assets/js/plugin/chart.js/chart.min.js"></script>
+<!-- Chart JS -->
+<script src="assets/js/plugin/chart.js/chart.min.js"></script>
 
-    <!-- jQuery Sparkline -->
-    <script src="assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+<!-- jQuery Sparkline -->
+<script src="assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
 
-    <!-- Chart Circle -->
-    <script src="assets/js/plugin/chart-circle/circles.min.js"></script>
+<!-- Chart Circle -->
+<script src="assets/js/plugin/chart-circle/circles.min.js"></script>
 
-    <!-- Datatables -->
-    <script src="assets/js/plugin/datatables/datatables.min.js"></script>
+<!-- Datatables -->
+<script src="assets/js/plugin/datatables/datatables.min.js"></script>
 
-    <!-- jQuery Vector Maps -->
-    <script src="assets/js/plugin/jsvectormap/jsvectormap.min.js"></script>
-    <script src="assets/js/plugin/jsvectormap/world.js"></script>
+<!-- jQuery Vector Maps -->
+<script src="assets/js/plugin/jsvectormap/jsvectormap.min.js"></script>
+<script src="assets/js/plugin/jsvectormap/world.js"></script>
 
-    <!-- Sweet Alert -->
-    <script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+<!-- Sweet Alert -->
+<script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
 
-    <!-- Kaiadmin JS -->
-    <script src="assets/js/kaiadmin.min.js"></script>
+<!-- Kaiadmin JS -->
+<script src="assets/js/kaiadmin.min.js"></script>
 
-    <!-- Kaiadmin DEMO methods, don't include it in your project! -->
-    <script src="assets/js/setting-demo.js"></script>
-    <script src="assets/js/demo.js"></script>
-    <script>
-      $("#lineChart").sparkline([102, 109, 120, 99, 110, 105, 115], {
-        type: "line",
-        height: "70",
-        width: "100%",
-        lineWidth: "2",
-        lineColor: "#177dff",
-        fillColor: "rgba(23, 125, 255, 0.14)",
-      });
+<!-- Kaiadmin DEMO methods, don't include it in your project! -->
+<script src="assets/js/setting-demo.js"></script>
+<script src="assets/js/demo.js"></script>
+<script>
+  $("#lineChart").sparkline([102, 109, 120, 99, 110, 105, 115], {
+    type: "line",
+    height: "70",
+    width: "100%",
+    lineWidth: "2",
+    lineColor: "#177dff",
+    fillColor: "rgba(23, 125, 255, 0.14)",
+  });
 
-      $("#lineChart2").sparkline([99, 125, 122, 105, 110, 124, 115], {
-        type: "line",
-        height: "70",
-        width: "100%",
-        lineWidth: "2",
-        lineColor: "#f3545d",
-        fillColor: "rgba(243, 84, 93, .14)",
-      });
+  $("#lineChart2").sparkline([99, 125, 122, 105, 110, 124, 115], {
+    type: "line",
+    height: "70",
+    width: "100%",
+    lineWidth: "2",
+    lineColor: "#f3545d",
+    fillColor: "rgba(243, 84, 93, .14)",
+  });
 
-      $("#lineChart3").sparkline([105, 103, 123, 100, 95, 105, 115], {
-        type: "line",
-        height: "70",
-        width: "100%",
-        lineWidth: "2",
-        lineColor: "#ffa534",
-        fillColor: "rgba(255, 165, 52, .14)",
-      });
-    </script>
+  $("#lineChart3").sparkline([105, 103, 123, 100, 95, 105, 115], {
+    type: "line",
+    height: "70",
+    width: "100%",
+    lineWidth: "2",
+    lineColor: "#ffa534",
+    fillColor: "rgba(255, 165, 52, .14)",
+  });
+</script>
 </body>
 
 @yield('scripts')
